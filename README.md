4c861ee1cbf8da8763303836691900a6319ab2d2
ehsan <ehsan.mujahid1798@gmail.com>
2023-02-25 16:33:13 +0400

    Uploading local work before github repo

--- README.md
+++ README.md
@@ -1,2 +1,203 @@
-# makeWorkflows
-a personalised menu-based workflow script  
+ This is a gargantuan task ! Separate it into stages, we are making the final one | 2 Dec 1839
+
+NOTE:
+	While I might use different words, some things will produce the same workflow,
+	though I might induce different intents and restrict focus anyways.
+
+	An example would be "Read for Fun" and "Read IT" as both may just open the library app.
+
+# STAGE 1 - PoC and Basics
+-[x] get the university stuff working
+-[x] cycle courses
+-[x] select course
+-[x] course becomes arg for 3 commands
+-[x] exits. 
+
+# STAGE 2 - Skeleton Rising
+-[x] establish a git for this
+-[x] create a subdirectory for this
+-[] furnish the .gitignore
+-[] ensure I use the gits, and upload to Github every day
+-[x] fill out the Top 5+2 base options
+-[x] incorporate university function as a sub-option
+-[c] build an array unpacker
+-[c] possibly model the arrays with 0 as the name of the parent option.
+-[x] have it echo each one's name
+-[] collate important commands in .playbook
+-[x] asks to exit
+
+# STAGE 3 - THE UI/UX STAGE
+-[x] pretty up the sourcing
+-[x] pretty up function calling
+-[] pretty up the Template Pattern
+-[x] add logging of selections selected
+-[c] add colour and ASCII to UI
+-[c] Add a progress bar during the opening
+
+# STAGE 4 - The Meat and Potatoes
+-[] incorporate Read & Note functionality
+-[] open Calibre Library
+-[] echo other scripts
+-[x] incorporate Watch functuonality
+-[x] open firefox with specific URLs in mind | ENSURE WE SHOWCASE THE URL
+-[x] incorporate Playtime functionality
+-[x] add steam games | just a static adding of local gameids for now
+-[x] incorporate Code & Do functionality
+-[x] cycle scripts  | 3 terminal tabs and firefox
+-[x] cycle projects | VsCode, Spotify, and Firefox
+-[] open Gimp
+-[] open site/app
+-[] incorporate Write functionality
+-[] open Obsidian or Gedit, vim can go to code.
+
+# STAGE 5 - ADVANCED EXTERNAL CODING
+-[x] add steam game functionality (dynamic)
+-[] add YouTube API functionality (the daily curl)
+-[] attempt columns
+-[] add buffet functionality
+
+# STAGE 6
+-[] finish it
+
+
+# unassigned
+-[x] timed gameplay
+-[] timed watching (let browse be open, but watch time should be monitored)
+-[x] every page should have lastpicked
+-[x] exit or return works
+-[] watch: grep a page of show names and specific URLs for them
+
+# other
+
+Five + 2 base options | 10 Feb 1920 added listen as the 6th
+	1. Read
+		i) University
+			...) however many courses in there
+		ii) Library (Browse)
+		iii) Library (Specific)
+			...) any - not all local, some firefox
+		iiii) Reading for fun (Browse)
+		iiiii) Notes 
+			a) (echo "run makeNotes")
+			b) (echo "check notes")
+		iiiiii) Email
+		iiiiiii) News Sites
+		iiiiiiii) Anki
+	2. Work
+		i) General Programming (VScode +)
+			a) new project
+			...) cycle through existing
+		ii) Scripting Automation (.scripts/, 3 tabs, vim and gedit)
+			a) new project | calls .scripts/makeNewScript.sh
+			...) cycle through existing
+		iii) General Graphic Design (Gimp)
+		iiii) Pixel Graphic Design (Site or compile that app )
+		iiiii)
+	3. Play
+		i) Lutris (Browse)
+		ii) Lutris
+			...) specific games
+		iii) Steam (Browse)
+		iiii) Steam
+			...) steam steam://rungameid/{YourGameID}
+		iiiii) Minecraft
+	4. Watch
+		i) YouTube (Browse)
+		ii) Youtube (Specific)
+			...) daily videos from API.
+		iii) PrimeVideo
+			... specific content too
+		iiii) Twitch ?
+	5. Write
+		i) Albanian practice | open vim and that website | also, think about other languages maybe ?
+		ii) Story writing
+		iii) Garbage time | GarbageStream.md
+		iiii) Fantasy writing
+		iiiii) Attribute writing
+		iiiiii) Blog writing
+		iiiiiii) Idea Dumping
+		iiiiiiii) Todo Writing
+	6. Listen
+		i) Spotify
+			... maybe async ?
+		ii) Discord
+		iii) Local Podcasts
+	n. Buffet
+		...) alllll the options | Specific ones still take a second click
+	n+1. Shush (exit)
+
+
+
+#		Array
+maybe use arrays to store the commands
+for unpacking too
+
+#		CHECK UPDATES
+ this runs a check for updates for all apps here.
+calibre library
+Lutris
+Steam
+Spotify
+VSCode
+Minecraft
+vim
+
+
+#		EASY (A/E)DDITION
+ REMEMBER, THESE MUST BE EASILY EDITABLE
+ OUR PRIMEVIDEO SUBSCRIPTION WILL EXPIRE SOON
+ AND WE MAY USE DIFFERENT SOFTWARE
+ JUST ASSUME WE CAN GENERALISE THE COMMANDS TO VARS
+ THIS WAY
+ WE CAN HAVE THE ORIGINAL CODE BE WORKING
+ AND ALL WE'D HAVE TO DO IS USE ADAPTORS
+
+
+#		BUFFET OPTION
+ IN THAT, ALL PROGRAMS CAN BE OPENED
+ SELECT ONE, IT WILL ASK AGAIN AFTER ECHOING SELECTION
+ SELECT AGAIN TILL COMPLETION
+ OPEN ALL OPTIONS
+
+
+#		MAY ASK TO EXIT
+ MAY ALSO ASK ADDITIONAL APPLICATIONS
+ THIS BRINGS THE USER TO THE BUFFET OPTION
+
+
+#		LOG SELECTIONS
+ should log the selections
+ which I _could_ use to automatically display code
+ that should be an alias
+
+
+#		EXTERNAL SCRIPTS
+ have this call other scripts to do it. 
+ save those scripts in a subfolder of .scripts/
+ WE COULD STORE ALL NON-TEMPLATE CODE IN EXTERNAL .sh or even alias
+ AND ENSURE IF ERROR IN SOURCE, STATE AS SO WITHOUT EXITING
+ STORING USEFUL CODE ELSEWHERE WOULD ALSO ALLOW AUTOMATION OUTSIDE
+ i.e., I CAN SCROLL THROUGH THE ALIASES WITHOUT FIDDING HERE IF I KNOW WHAT I MUST DO
+
+
+ #		Alphabetised ?
+ sort sub-commands alphabetically ?
+
+
+#		BACK BUTTON
+ go back.
+
+
+#		Colour and Columns ?
+ pretty...
+
+#		DO-NOTHINGS
+ not everything needs to be fully automated.
+ rely on echos for most of it, and slowly (semi/fully)-automate tasks
+ some may still be echos e.g., podcasts
+
+
+#		PODCASTS
+ should echo "use the bluetoothz"
+ and 	 echo "I can't automate phone tasks - yet..."
+
---
+++ makeWorkflow.sh
@@ -0,0 +1,416 @@
+#!/bin/bash
+
+#########################
+#	conventions     #
+#########################
+
+# for success  ✔️ :use ${passStr}
+# for failure  ❌:use ${failStr}
+# for datetime   :use ${datetime} : uses ISO 8601
+
+# "String" for statements
+# " -string(...)" for processes / start of them
+# " --string" for sub-proceses
+
+# ToC
+# Conventions
+# Variables
+# Functions
+# Algorithm
+# Wind-up
+
+#########################
+#	variables       #
+#########################
+
+START=$SECONDS
+passStr=" ✔️ "
+failStr=" ❌"
+datetime=$(date "+%Y-%m-%dT%T%:z")
+date=$(date "+%Y-%m-%d")
+
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+script_name="$(basename ${0})"
+
+clean_script_name="${script_name//".sh"/}"
+
+SOURCEDIR="$HOME/Scripts/${clean_script_name}"
+
+LASTPICKED="NONE"
+
+#################################
+#	errors and exits	#
+#################################
+
+
+function missing_error_exit(){
+	echo "ERROR::MISSING:: $1 ${failStr}"
+	exit_error
+}
+
+
+trap exit_script SIGINT
+
+
+function exit_script(){
+
+	END=$SECONDS
+
+	logger "ran ${script_name} successfully for $((END-START))s at $(date)"
+
+        echo -e "\n - ending ${script_name}..."
+        echo -e "\nMirupafshim, $USER."
+
+	exit 0
+}
+
+
+function exit_error(){
+	echo " - ending ${script_name}..."
+        echo "Mirupafshim, $USER."
+	exit 1
+}
+
+
+#########################
+#	functions	#
+#########################
+
+
+function say_hello {
+	echo -e "\nMirë se erdhet, $USER, në ${script_name} V1.43\n"
+
+	return 0
+}
+
+
+function inform_topic {
+	LASTPICKED="${1}"
+	clear
+	return 0
+}
+	
+
+function inform_subtopic {
+	clear
+	LASTPICKED="${LASTPICKED}/${1}"
+
+	return 0
+}
+
+
+function prepare_source {
+	# this should ideally replace the source-ing logic
+        sourcefile="${SOURCEDIR}/${1}"
+
+        if ! [ -a ${sourcefile} ]; then
+                echo " - ERROR:: SOURCE FILE ABSENT ${failStr}"
+                return 2
+        elif ! [ -s ${sourcefile} ]; then
+                echo " - ERROR:: SOURCE FILE EMPTY ${failStr}"
+                return 3
+        fi
+
+        source ${sourcefile}
+
+	return 0
+}
+
+
+function start_procedure_university(){
+
+	WORKFLOWVARFILE="university_workflows.var"
+	prepare_source "${WORKFLOWVARFILE}" || echo " HALLO "
+	initiate_procedure_university
+
+	return 0
+}
+
+
+function start_procedure_browse(){
+	
+	WORKFLOWVARFILE="browse_workflows.var"
+	prepare_source "${WORKFLOWVARFILE}" || echo " HALLO "
+	initiate_procedure_browse
+
+	return 0
+}
+
+
+function start_procedure_watch(){
+	
+	WORKFLOWVARFILE="watch_workflows.var"
+	prepare_source "${WORKFLOWVARFILE}" || echo " HALLO "
+	initiate_procedure_watch
+
+	return 0
+}
+
+
+function start_procedure_play(){
+
+	WORKFLOWVARFILE="play_workflows.var"
+	prepare_source "${WORKFLOWVARFILE}" || echo " HALLO "
+	initiate_procedure_play
+
+	return 0
+}
+
+
+function start_procedure_write(){
+
+	WORKFLOWVARFILE="write_workflows.var"
+	prepare_source "${WORKFLOWVARFILE}" || echo " HALLO "
+	initiate_procedure_write
+
+	return 0
+}
+
+
+function start_procedure_read(){
+
+	WORKFLOWVARFILE="read_workflows.var"
+	prepare_source "${WORKFLOWVARFILE}" || echo " HALLO "
+	initiate_procedure_read
+
+	return 0
+}
+
+
+function start_procedure_listen(){
+	WORKFLOWVARFILE="listen_workflows.var"
+	prepare_source "${WORKFLOWVARFILE}" || echo " HALLO "
+	initiate_procedure_listen
+
+	return 0
+}
+
+
+function start_procedure_work(){
+
+	WORKFLOWVARFILE="work_workflows.var"
+	prepare_source "${WORKFLOWVARFILE}" || echo " HALLO "
+	initiate_procedure_work
+
+	return 0
+}
+
+
+function start_procedure_cycler(){
+
+	WORKFLOWVARFILE="cycler_workflows.var"
+	prepare_source "${WORKFLOWVARFILE}" || echo " HALLO "
+	initiate_procedure_cycler ${1}
+
+	return 0
+
+}
+
+
+function start_procedure_WIP(){
+	clear
+	say_hello || echo "hi"
+	echo " - Do this yourself, ya big bum"
+	echo " -- returning..."
+	sleep 1
+
+	return 0
+}
+
+
+function start_subprocedure_WIP(){
+	echo " - Do this yourself, ya big bum"
+	sleep 1
+	return 0
+}
+
+
+function start_procedure_temp(){
+
+	clear
+	say_hello || echo "A quick hai !"
+
+	echo " - TEMPORARY PROCEDURE"
+	echo " -- This task becomes a cycler as soon as we can pin procedures"
+	echo " -- We will then have the temp be of something else"
+	echo " - Try Obsidian"
+	echo " - Continue ? ( Any Key )"
+	read
+	echo " -- Opening .MD..."
+	vim "$HOME/Attributes/career.md"
+
+	return 0
+}
+
+#########################
+#	algorithm       #
+#########################
+
+
+while [ 1 ]; do
+	
+	clear
+	say_hello
+
+	# TODO: use this everywhere
+	#gnome-terminal --wait --tab -- bash -c "command"
+
+	declare -a menuArray
+	declare -A tempArray
+
+#       list choices
+	
+	READ="Read"
+	WORK="Work"
+	PLAY="Play"
+	WATCH="Watch"	
+	BROWSE="Browse"	
+	WRITE="Write"
+	LISTEN="Listen"
+
+	PROJECTS="[DIR]	Projects"
+	SCRIPTS="[DIR]	Scripts"
+	ATTRIBUTES="[DIR]	Attributes"
+	AGENDA="[DIR]	Agenda"
+
+	BUFFET="Buffet		(WIP)"
+
+	TEMP="Define Careers"
+	EXITTEXT="Exit"
+	RETURNTEXT="Return"
+
+#	We need to dynamise the ordering, such that we only need to
+#	initialise the associative array and leave the numerical ordering
+#	at runtime - except for the exit clause.
+
+#	menuArray[]="${READ}"
+#	menuArray[]="${WORK}"
+#	menuArray[]="${PLAY}"
+#	menuArray[]="${BROWSE}"
+#	menuArray[]="${WATCH}" 
+#	menuArray[]="${WRITE}"
+#	menuArray[]="${LISTEN}"
+#	menuArray[]="${PROJECTS}"
+#	menuArray[]="${SCRIPTS}"
+#	menuArray[]="${ATTRIBUTES}"
+#	menuArray[]="${AGENDA}"
+#	menuArray[]="${BUFFET}"
+
+	menuArray[0]="${READ}"
+	menuArray[1]="${WORK}"
+	menuArray[2]="${PLAY}"
+	menuArray[3]="${BROWSE}"
+	menuArray[4]="${WATCH}" 
+	menuArray[5]="${WRITE}"
+	menuArray[6]="${LISTEN}"
+	menuArray[7]="${PROJECTS}"
+	menuArray[8]="${SCRIPTS}"
+	menuArray[9]="${ATTRIBUTES}"
+	menuArray[10]="${AGENDA}"
+	menuArray[11]="${BUFFET}"
+
+
+# needs to be generated.
+
+	count="${#menuArray[*]}"
+
+	TEMPCLAUSE="${count}"
+	count="$(( count + 1 ))"
+	menuArray["${TEMPCLAUSE}"]="${TEMP}"
+
+#       return-clause
+	EXITCLAUSE="${count}"
+	menuArray["${EXITCLAUSE}"]="${EXITTEXT}"
+	count="$(( count + 1 ))"
+
+	echo " - LAST PICKED: ${LASTPICKED}"
+
+	echo " - OPTIONS: "
+
+	for (( i=0; i<${count}; i++ )); do
+		echo " -- ${i}. ${menuArray[${i}]}"
+	done
+
+        while [ 1 ]; do
+                echo -e "\n - So, whadaya doin' ?"
+                read -p " -- Choice: " choice
+
+                if [[ -n "${choice}" ]] && [[ "${choice}" -lt "${count}" ]]; then
+                        break
+                fi
+
+                echo " -- ${choice} is invalid ${failStr}"
+
+        done
+
+	topic="${menuArray[${choice}]}"
+
+	case $topic in
+	"${READ}")
+		inform_topic "$topic"
+		start_procedure_read
+		;;
+	"${WORK}")
+		inform_topic "$topic"
+		start_procedure_work
+		;;
+	"${PLAY}")
+		inform_topic "$topic"
+		start_procedure_play
+		;;
+	"${WATCH}")
+		inform_topic "$topic"
+		start_procedure_watch
+		;;
+	"${BROWSE}")
+		inform_topic "$topic"
+		start_procedure_browse
+		;;
+	"${WRITE}")
+		inform_topic "${topic}"
+		start_procedure_write
+		;;
+	"${LISTEN}")
+		inform_topic "$topic"
+		start_procedure_listen
+		;;
+	"${TEMP}")
+		inform_topic "$topic"
+		start_procedure_temp
+
+		;;
+	"${PROJECTS}"|"${SCRIPTS}"|"${ATTRIBUTES}"|"${AGENDA}")
+		inform_topic "$topic"
+		start_procedure_cycler "$HOME/${topic}"		
+
+		;;
+
+	"${BUFFET}")
+#		inform_topic "$topic"
+		start_procedure_WIP "${topic}"
+
+		;;
+	"${EXITTEXT}")
+		exit_script
+
+		;;
+	*)
+		echo "Sorry, $topic is not in my vocabulary..."
+		;;
+	esac
+
+	unset menuArray	
+	
+done
+
+
+#########################
+#	wind-up		#
+#########################
+
+exit_script
+
+exit 0 
